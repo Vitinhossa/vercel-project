2c1112030edc573a19d961c76f9a12816da92526
Vitinhossa <vitorpinto1505@icloud.com>
2024-03-26 17:18:09 -0300

    Update index.tsx

--- src/pages/transferencia/10x/index.tsx
+++ src/pages/transferencia/10x/index.tsx
@@ -1,9 +1,13 @@
+// src/pages/transferencia/10x/index.tsx
+import React from 'react';
 import ContentContainer from '@/components/atoms/ContentContainer';
+// Importa o template de transferência usando o alias @
 import Transferencia10xTemplate from '@/components/templates/transferencia10xTemplate';
 
 export default function Transferencia10x() {
   return (
     <ContentContainer>
+      {/* Utiliza o template importado no seu componente */}
       <Transferencia10xTemplate />
     </ContentContainer>
   );
