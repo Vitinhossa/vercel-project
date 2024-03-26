33359b478fcb5adc24c7291f04b510980117f724
Vitinhossa <vitorpinto1505@icloud.com>
2024-03-26 17:30:52 -0300

    yes

---
+++ src/components/templates/transferencia10xTemplate.tsx
@@ -0,0 +1,11 @@
+import Header from '../molecules/Header/index';
+import TransferenciaContent from '../molecules/TransferenciaContent';
+
+export default function TransferenciaTemplate() {
+  return (
+    <>
+      <Header title="Transferência" justifyContent="flex-start" />
+      <TransferenciaContent />
+    </>
+  );
+}
--- src/pages/transferencia/10x/index.tsx
+++ src/pages/transferencia/10x/index.tsx
@@ -1,14 +1,15 @@
 // src/pages/transferencia/10x/index.tsx
+
 import React from 'react';
 import ContentContainer from '@/components/atoms/ContentContainer';
-// Importa o template de transferência usando o alias @
 import Transferencia10xTemplate from '@/components/templates/transferencia10xTemplate';
 
-export default function Transferencia10x() {
+const Transferencia10xPage = () => {
   return (
     <ContentContainer>
-      {/* Utiliza o template importado no seu componente */}
       <Transferencia10xTemplate />
     </ContentContainer>
   );
 }
+
+export default Transferencia10xPage;
